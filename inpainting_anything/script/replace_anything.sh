718f937e818eaaa4c3e75ac6beeca16f06a732b6
Yeonghun Jeong <jyh11224@donga.ac.kr>
2024-11-14 20:35:31 +0900

    Update replace_anything.sh

--- inpainting_anything/script/replace_anything.sh
+++ inpainting_anything/script/replace_anything.sh
@@ -1,15 +1,11 @@
 #!bin/bash
 
-for image in input/train4inpainting/snow_images/*.jpg
-do
-    python3 replace_anything.py \
-        --input_img ${image} \
-        --coords_type click \
-        --point_coords 550 350 \
-        --point_labels 2 \
-        --text_prompt "a hill of snow" \
-        --output_dir ./results/snow_images \
+python3 replace_anything.py \
+        --input_img  "/workspace/Inpaint-Anything/example/shoes.png"\
+        --detect "person" \ 
+        --point_labels 5 \
+        --text_prompt "shoes" \
+        --output_dir ./results/shoes \
         --sam_model_type "vit_h" \
         --sam_ckpt ./weights/sam_vit_h_4b8939.pth \
         --seed 30
-done
